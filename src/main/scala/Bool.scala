b3d6e1bcccb4df56b409e0d55187688c65de15fa
Rahul Madhavan <rahul@journeyoflove.in>
2019-05-04 10:29:01 +0530

    working Bool class

--- src/main/scala/Bool.scala
+++ src/main/scala/Bool.scala
@@ -1,22 +1,29 @@
-package idealized.scala
 
-abstract class Boolean {
+object tru extends Bool {
+  def ifthenelse[T](t: => T, e: => T) = t
+}
+
+object fal extends Bool {
+  def ifthenelse[T](t: => T, e: => T) = e
+}
+
+
+abstract class Bool {
 
   def ifthenelse[T](thn : => T,els: => T): T
 
   //  call as A && B
-  def &&(B: => Boolean):Boolean = ifthenelse(B,false)
+  def &&(B: => Bool):Bool = ifthenelse(B,fal)
 
   //  call as A || B
-  def ||(B: => Boolean):Boolean = ifthenelse(true,B)
+  def ||(B: => Bool):Bool = ifthenelse(tru,B)
 
   //  call as ! Ax
-  def unary_! : Boolean = ifthenelse(false,true)
+  def unary_! : Bool = ifthenelse(fal,tru)
 
-  def ==(x:Boolean):Boolean = ifthenelse(x,x.unary_!)
+  def ==(x:Bool):Bool = ifthenelse(x,x.unary_!)
 
-  def !=(x:Boolean):Boolean = ifthenelse(x.unary_!,x)
-
-
+  def !=(x:Bool):Bool = ifthenelse(x.unary_!,x)
 
 }
+
