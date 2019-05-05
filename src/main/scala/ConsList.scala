34ab322cc0322bf3e779d44e9df52fa556078859
Rahul Madhavan <rahul@journeyoflove.in>
2019-05-05 11:02:15 +0530

    changed names of cons and nil to mycons and mynil in ConsList as conflicts with TweetSet

--- src/main/scala/ConsList.scala
+++ src/main/scala/ConsList.scala
@@ -12,7 +12,7 @@
  *   scala> import ConsList._
      import ConsList._
 
-     scala> val a = new cons[Int](1,new nil[Int])
+     scala> val a = new cons[Int](1,new mynil[Int])
      a: ConsList.cons[Int] = ConsList$cons@719d868
 
      scala> val b = new cons[Int](2,a)
@@ -28,91 +28,94 @@
  *
  */
 
-trait List[T] {
+trait MyList[T] {
   def isEmpty: Boolean
 
   def head: T
 
-  def tail: List[T]
+  def tail: MyList[T]
 }
 
-class cons[T](val head: T, val tail: List[T]) extends List[T] {
+class mycons[T](val head: T, val tail: MyList[T]) extends MyList[T] {
   override def isEmpty: Boolean = false
 
   /*    The below are not required because we have already defined val in the arguments list
   *     val is a special case of a def or function definition
   *     def head:T = head
-  *     def tail:List[T] = tail
+  *     def tail:MyList[T] = tail
   */
 }
 
-class nil[T] extends List[T] {
+class mynil[T] extends MyList[T] {
   override def isEmpty: Boolean = true
 
-  def head: Nothing = throw new NoSuchElementException("This is a nil element with no head")
+  def head: Nothing = throw new NoSuchElementException("This is a mynil element with no head")
 
-  def tail: Nothing = throw new NoSuchElementException("This is a nil element with no tail")
+  def tail: Nothing = throw new NoSuchElementException("This is a mynil element with no tail")
 }
 
-// we can use a method like the below to create a list
-def singleton[T](elem: T): List[T] = {
-  new cons[T](elem, new nil[T])
-}
-/*
-* Write a function nth that takes an integer n and a list and selects the n'th element of the list.
-* Elements are numbered from 0.
-* If index is outside the range from 0 up to the length of the list minus one, a IndexOutOfBoundsException should be thrown.
-*
-* scala> val a = new cons[Int](1,new nil[Int])
-  a: ConsList.cons[Int] = ConsList$cons@11bd984d
-
-  scala> val b = new cons[Int](2,a)
-  b: ConsList.cons[Int] = ConsList$cons@1af29814
-
-  scala> val c = new cons[Int](3,b)
-  c: ConsList.cons[Int] = ConsList$cons@46f78f67
-
-* scala> nth(0,c)
-  res6: Int = 3
-
-  scala> nth(1,c)
-  res7: Int = 2
-
-  scala> nth(2,c)
-  res8: Int = 1
-*
-*
-*/
-def nth[T](n: Integer, mylist: List[T]): T = {
-  def loop[T](current: Integer, mylist: List[T]): T = {
-    if (mylist.isEmpty || n < 0) throw new IndexOutOfBoundsException("Please check the index")
-    else if (current == 0) mylist.head
-    else
-      loop(current - 1, mylist.tail)
+object MyListOperations {
+  // we can use a method like the below to create a list
+  def singleton[T](elem: T): MyList[T] = {
+    new mycons[T](elem, new mynil[T])
   }
 
-  loop(n, mylist)
+  /*
+  * Write a function nth that takes an integer n and a list and selects the n'th element of the list.
+  * Elements are numbered from 0.
+  * If index is outside the range from 0 up to the length of the list minus one, a IndexOutOfBoundsException should be thrown.
+  *
+  * scala> val a = new mycons[Int](1,new mynil[Int])
+    a: ConsList.mycons[Int] = ConsList@11bd984d
 
+    scala> val b = new mycons[Int](2,a)
+    b: ConsList.mycons[Int] = ConsList$mycons@1af29814
+
+    scala> val c = new mycons[Int](3,b)
+    c: ConsList.mycons[Int] = ConsList$mycons@46f78f67
+
+  * scala> nth(0,c)
+    res6: Int = 3
+
+    scala> nth(1,c)
+    res7: Int = 2
+
+    scala> nth(2,c)
+    res8: Int = 1
+  *
+  *
+  */
+  def nth[T](n: Integer, mylist: MyList[T]): T = {
+    def loop[T](current: Integer, mylist: MyList[T]): T = {
+      if (mylist.isEmpty || n < 0) throw new IndexOutOfBoundsException("Please check the index")
+      else if (current == 0) mylist.head
+      else
+        loop(current - 1, mylist.tail)
+    }
+
+    loop(n, mylist)
+
+  }
 }
 
-object List {
+object MyList {
   /*
-  List(1,2) = List.apply(1,2)
+  MyList(1,2) = MyList.apply(1,2)
   */
 
-  def apply[T](x:T,y:T): List[T] = new Cons(x, new Cons(y, new nil))
+  def apply[T](x:T,y:T): MyList[T] = new mycons(x, new mycons(y, new mynil))
 
   /*
-  List(1) = List.apply(1)
+  MyList(1) = MyList.apply(1)
   */
 
-  def apply[T](x:T): List[T] = new Cons(x, new nil)
+  def apply[T](x:T): MyList[T] = new mycons(x, new mynil)
 
   /*
-  List() = List.apply()
+  MyList() = MyList.apply()
   */
 
-  def apply[T](): List[T] = new nil
+  def apply[T](): MyList[T] = new mynil
 
 }
 
